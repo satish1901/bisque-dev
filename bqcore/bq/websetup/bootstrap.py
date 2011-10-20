fb210dc25dd0965c143832554d9c9195c689c2f6
Kristian Kvilekval <kris@cs.ucsb.edu>
2011-10-20 09:40:10 -0700

    typo in bootstrap.py

--- bqcore/bq/websetup/bootstrap.py
+++ bqcore/bq/websetup/bootstrap.py
@@ -53,7 +53,7 @@ def bootstrap(command, conf, vars):
         from  bq.data_service.model import Taggable, Tag, BQUser
         from bq.data_service.model import UniqueName
 
-        admin = model.DBSession.query(BQUser).filter_by(username = 'admin').first()
+        #admin = model.DBSession.query(BQUser).filter_by(username = 'admin').first()
         #admin.owner_id = admin.id
         
         system = model.DBSession.query(Taggable).filter_by (tb_id = UniqueName('system').id).first()
