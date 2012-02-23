93e1bce22f8517befffac04e18cda62526d253dc
Kristian Kvilekval <kris@cs.ucsb.edu>
2012-02-23 12:16:29 -0800

    mysql db migration changes

--- bqcore/migration/versions/003_data_migration.py
+++ bqcore/migration/versions/003_data_migration.py
@@ -33,39 +33,6 @@ def upgrade(migrate_engine):
     print "cleaning up"
     #END MIGRATION OF DATA
     
-    # Removing unused columns and tables
-    from migration.versions.model002 import (taggable, names, tags, gobjects, images,
-                                             users, groups, templates, modules, mex,
-                                             dataset, services)
-    from migration.versions.model002 import (files, files_acl)
-    from migration.versions.model002 import (values, vertices)
-
-    taggable.c.tb_id.drop()
-    taggable.c.mex_id.alter (name='mex_id')
-    values.c.parent_id.alter(name='resource_parent_id')
-    vertices.c.parent_id.alter(name='resource_parent_id')
-    # Indexes
-    doc_index = Index('resource_document_idx', taggable.c.document_id)
-    doc_index.create()
-    parent_index = Index('resource_parent_idx', taggable.c.resource_parent_id)
-    parent_index.create()
-    type_index = Index('resource_type_idx', taggable.c.resource_type)
-    type_index.create()
-
-    services.drop()
-    dataset.drop()
-    mex.drop()
-    modules.drop()
-    templates.drop()
-    groups.drop()
-    users.drop()
-    images.drop()
-    gobjects.drop()
-    tags.drop()
-    names.drop()
-
-    files_acl.drop()
-    files.drop()
 
 
 def downgrade(migrate_engine):
