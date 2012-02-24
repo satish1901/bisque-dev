69859eebbc8c5ee4d67447c6e61027a69a1d1d09
Kristian Kvilekval <kris@cs.ucsb.edu>
2012-02-23 16:14:49 -0800

    script for isam conversion.

--- bqcore/migration/versions/004_mysql_upgrade.sql
+++ bqcore/migration/versions/004_mysql_upgrade.sql
@@ -2,31 +2,42 @@
 BEGIN;
 
 
-alter table taggable drop foreign key taggable_ibfk_1;
-alter table taggable drop column tb_id;
+
+ALTER TABLE taggable drop foreign key taggable_ibfk_1;
+ALTER TABLE taggable drop column tb_id;
 
 
-alter table taggable change column mex mex_id;
-alter table values  change column  parent_id resource_parent_id;
-alter table vertices  change column  parent_id resource_parent_id;
 
-drop table services;
-drop table dataset;
-drop table mex;
-drop table modules;
-drop table templates;
-drop table groups;
-drop table users;
-drop table images;
-drop table gobjects;
-drop table tags;
-drop table names;
-drop table files_acl;
-drop table files;
+ALTER TABLE taggable drop foreign key taggable_ibfk_3;
+ALTER TABLE taggable change column mex mex_id int(11);
+ALTER TABLE taggable ADD CONSTRAINT  FOREIGN KEY (mex_id) REFERENCES taggable(id);
 
-create index resource_document_idx on taggable (document_id);
-create index resource_parent_idx on taggable (resource_parent_id);
-create index resource_type_idx on taggable (resource_type);
+ALTER TABLE `values` drop foreign key values_ibfk_2;
+ALTER TABLE `values`  change column  parent_id resource_parent_id int(11);
+ALTER TABLE `values` ADD CONSTRAINT FOREIGN KEY (resource_parent_id) REFERENCES taggable(id);
+
+
+alter table vertices drop foreign key vertices_ibfk_1;
+alter table vertices  change column  parent_id resource_parent_id int(11);
+alter table vertices ADD CONSTRAINT FOREIGN KEY (resource_parent_id) REFERENCES taggable(id);
+
+DROP TABLE service;
+DROP TABLE datasets;
+DROP TABLE mex;
+DROP TABLE modules;
+DROP TABLE templates;
+DROP TABLE groups;
+DROP TABLE users;
+DROP TABLE images;
+DROP TABLE gobjects;
+DROP TABLE tags;
+DROP TABLE names;
+DROP TABLE file_acl;
+DROP TABLE files;
+
+CREATE INDEX resource_document_idx on taggable (document_id);
+CREATE INDEX resource_parent_idx on taggable (resource_parent_id);
+CREATE INDEX resource_type_idx on taggable (resource_type);
 
 commit;
 
