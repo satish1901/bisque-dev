ea131f99288c825c29eb1292fb111d64f376c552
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2012-04-06 17:08:14 -0700

    nd3d confidence in percentages

--- modules/NuclearDetector3D/NuclearDetector3D.m
+++ modules/NuclearDetector3D/NuclearDetector3D.m
@@ -61,7 +61,7 @@ function NuclearDetector3D(mex_url, access_token, image_url, nuclear_channel, nu
                 p = g.addGobject('nucleus', int2str(j*i));        
                 v = [np{j}(i,2)-1, np{j}(i,1)-1, np{j}(i,3)-1, j-1.0];
                 p.addGobject('point', 'centroid', v );
-                p.addTag('confidence', np{j}(i,5), 'number'); 
+                p.addTag('confidence', np{j}(i,5)*100, 'number'); 
             end
         end
         
