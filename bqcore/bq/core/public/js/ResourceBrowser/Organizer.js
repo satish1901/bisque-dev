eba7d430e74249f6917c5f0dacffb6a06f2f1139
Utkarsh <Utkarsh@TheTruth.ece.ucsb.edu>
2012-04-13 11:44:43 -0700

    Ticket:547 Tag values were being fetched with old wpublic instead of the changed one. However, the data was being fetched with the correct wpublic.

--- bqcore/bq/core/public/js/ResourceBrowser/Organizer.js
+++ bqcore/bq/core/public/js/ResourceBrowser/Organizer.js
@@ -192,7 +192,7 @@ Ext.define('Bisque.ResourceBrowser.Organizer',
         {
             child.grid.setLoading({msg:''});
             var query = this.GetTagQuery();
-            var uri = this.dataset + '?{0}=' + child.tag + '&wpublic=' + this.wpublic + (query.length?'&tag_query='+query:'');
+            var uri = this.dataset + '?{0}=' + child.tag + '&wpublic=' + this.parent.wpublic + (query.length?'&tag_query='+query:'');
             var uri = Ext.String.format(uri, (child.tagType=='tag'?'tag_values':'gob_names'));
             
             BQFactory.load(uri, callback(this, 'PopulateGrid', true, child));
