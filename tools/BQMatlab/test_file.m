a9ac71b65ed24c04ef1b36b9c92b07d1c812cca9
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2012-04-27 19:36:06 -0700

    matlab api updated with file upload

--- tools/BQMatlab/test_file.m
+++ tools/BQMatlab/test_file.m
@@ -3,9 +3,24 @@ user = '';
 pass = '';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% using bq.File
+% fetching a file from Bisque
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 file = bq.Factory.make(url, [], user, pass);
+% fetch a file with the original file name to the current location
 fn = file.fetch( [] );
 
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% fetching a file from Bisque
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+url = 'http://vidi.ece.ucsb.edu:9090';
+user = 'XXX';
+pass = 'XXX';
+
+%filename = 'gtrain.model';
+filename = 'pr-1z.tif';
+file = bq.File.store(filename, url, user, pass); 
+
+file.addTag('about', 'this is a file upload from Matlab API');
+file.save();
