9f6be04c68e97c0378afa3fc490a59de690efaa2
Kristian Kvilekval <kris@cs.ucsb.edu>
2012-05-23 18:00:29 -0700

    simple check for modules before registration

--- bqengine/bq/engine/controllers/adapters/runtime_adapter.py
+++ bqengine/bq/engine/controllers/adapters/runtime_adapter.py
@@ -77,19 +77,32 @@ class RuntimeAdapter(BaseAdapter):
     def __init__(self):
         pass
     def check(self, module):
-        #module_name = module.get('name')
+        module_name = module.get('name')
         #module_path = module.get('path').split(' ')[-1]
         #command = os.path.join(MODULE_BASE, module_name, module_path)
         #if not os.path.exists (command):
         #    log.debug ("Failed to find module at %s" % command)
         #    return False
+
+        current_dir = os.getcwd()
+        try:
+            module_dir = os.path.join(MODULE_BASE, module_name)
+            log.info ("Currently in %s" % current_dir)
+            log.info ("Checking %s in %s" % (module_name,  module_dir))
+
+            os.chdir(module_dir)
+            m = ModuleRunner()
+            if not m.check (module_tree = module):
+                return False
         
-        async =  module.xpath('//tag[@name="asynchronous"]')
-        if len(async):
-            async[0].set('value', 'True')
-        else:
-            module.append(etree.Element('tag', name='asynchronous', value='True'))
-        return True
+            async =  module.xpath('//tag[@name="asynchronous"]')
+            if len(async):
+                async[0].set('value', 'True')
+            else:
+                module.append(etree.Element('tag', name='asynchronous', value='True'))
+            return True
+        finally:
+            os.chdir(current_dir)
 
     def execute(self, module, mex):
         log.debug ("module : " + etree.tostring(module))
