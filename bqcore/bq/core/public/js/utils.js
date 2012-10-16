96ca7db31f111e1c609b204da5ee1c7f9af567b3
Utkarsh Gaur <utkarsh.gaur@gmail.com>
2012-10-16 16:23:43 -0700

    Fixed utils.js missing some required functions. Added new js files to grunt.js

--- bqcore/bq/core/public/js/utils.js
+++ bqcore/bq/core/public/js/utils.js
@@ -1,3 +1,13 @@
+// ADD SOME GLOBAL VARS HERE TO KEEP OUR FILE LIST SHORT
+// for conformity sake:
+// paths will NOT end in a '/' slash
+//var server = 'arno.ece.ucsb.edu:8080';
+//var fullurl = server  + '/bisquik';
+
+var svgns  = "http://www.w3.org/2000/svg";
+var xlinkns  = "http://www.w3.org/1999/xlink";
+var xhtmlns = 'http://www.w3.org/1999/xhtml';
+
 // <![CDATA[
 
 // Create a method callback on a javascript objects.
@@ -45,6 +55,11 @@ function getY(obj) {
         obj = obj.offsetParent;
     }
     return curtop;
+}
+
+function getObj(name)
+{
+    return document.getElementById(name);
 }
 
 function attribDict(node) {
