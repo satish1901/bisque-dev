1f7f14caab9b935328690f8f488ade4c5ee39404
Kristian Kvilekval <kris@cs.ucsb.edu>
2013-01-17 13:52:19 -0800

    bisque_setup find defaults, bq-admin module (unregister, list-engine, list-server) new commands

--- bqserver/bq/module_service/commands/module_admin.py
+++ bqserver/bq/module_service/commands/module_admin.py
@@ -51,6 +51,7 @@ import optparse
 import os
 import logging
 import urlparse
+import urllib
 import datetime
 from lxml import etree
 from paste.deploy import appconfig
@@ -70,13 +71,19 @@ def load_config(filename):
 
 log = logging.getLogger('bq.engine.command.module_admin')
 
+usage = """
+%prog module [register|unregister/list-engine/list-server] [-u user:pass] [-p] [-a] http://myengnine.org/engine_service/[MyModule] [http://bisque_server/data_service/module_uri]
+
+  Register or unregister and bisque analysis (Engine) on a bisque server.
+
+  The module engine uri must be specified
+  Optionaly include the the module_uri on the server to disambiguate
+"""
+
 class module_admin(object):
     desc = 'module options'
     def __init__(self, version):
-        parser = optparse.OptionParser(
-                    usage="%prog module [register|unregister] [-u user:pass] [-p] [-a] http://myengnine.org/engine_service/[MyModule]",
-                    version="%prog " + version)
-
+        parser = optparse.OptionParser(usage=usage, version="%prog " + version)
         parser.add_option('-u', '--user', default=None, help='Login as user <user>:<pass>')
         parser.add_option('-a', '--all', action='store_true', help='Register/Unregister all modules at engine', 
                           default=False)
@@ -88,19 +95,23 @@ class module_admin(object):
         self.options = options
         self.command = None
         if len(self.args):
-            self.command = getattr(self, self.args.pop(0))
+            self.command = getattr(self, self.args.pop(0).replace('-', '_'), None)
         if not self.command:
-            parser.error("no command given")
+            parser.error("no valid command given")
 
         self.credentials = None
         if self.options.user:
             self.credentials = tuple(self.options.user.split(':'))
 
 
+        self.module_uri = None
         if len(self.args):
             self.engine_path = self.args.pop(0)
             if not self.engine_path.endswith('/'):
                 self.engine_path += '/'
+
+            if len(self.args):
+                self.module_uri = self.args.pop(0)
         else:
             parser.error('must provide URL to the service ')
 
@@ -139,7 +150,7 @@ class module_admin(object):
     def register_one(self, module_path):
         bisque_root = self.root
         module_path = norm(module_path + '/')
-        module_register = urlparse.urljoin(bisque_root, "module_service/register_engine")
+        module_register = norm (urlparse.urljoin(bisque_root, "module_service/register_engine") + '/')
         module_xml = self.get_xml( url = urlparse.urljoin(module_path, 'definition'))
         name = module_xml.get('name')
         if module_xml is not None:
@@ -152,10 +163,13 @@ class module_admin(object):
             if self.options.published:
                 for el in module_xml.getiterator(tag=etree.Element):
                     el.set ('permission', 'published')
-                    log.warn ('TAG %s' % el.tag)
             xml = etree.tostring(module_xml)
-            resp, content = http.xmlrequest (norm(module_register+'/'), method='POST', body=xml, 
-                                             userpass = self.credentials)
+            params = [ ('engine_uri', module_path) ]
+            if self.module_uri:
+                params.append ( ('module_uri', module_uri) )
+            url = "%s?%s" % (module_register, urllib.urlencode(params))
+            resp, content = http.xmlrequest (url, method='POST', body=xml, userpass=self.credentials)
+
             if resp.status == '401':
                 print "You do not have permission to register (provide credentials)"
             elif resp['status'] != '200':
@@ -190,8 +204,10 @@ class module_admin(object):
         module_unregister = norm (urlparse.urljoin(bisque_root, "module_service/unregister_engine") + '/')
 
         module_name = module_path.split('/')[-2]
-        url = "%s?name=%s" % (module_unregister, module_name)
-        
+        params = [ ('engine_uri', module_path) ]
+        if self.module_uri:
+            params.append ( ('module_uri', module_uri) )
+        url = "%s?%s" % (module_unregister, urllib.urlencode(params))
         resp, content = http.xmlrequest (url, method='GET', userpass=self.credentials)
         if resp.status == '401':
             print "You do not have permission to register (provide credentials)"
@@ -201,7 +217,51 @@ class module_admin(object):
             return
         print "UnRegistered"
         
-            
+    def list_engine(self):
+        if self.options.all:
+            module_paths = self.get_modules(self.engine_path)
+        else:
+            module_paths = [ self.engine_path ] 
+
+        for module in module_paths:
+            print module
+        
+
+    def list_server(self):
+        from collections import namedtuple
+        Row = namedtuple ('Row', ('name', 'engine', 'module'))
+        server_modules = self.get_xml( url = urlparse.urljoin(self.root, 'module_service'))
+
+        rows = [ Row(name=module.get('name'), engine=module.get('value'), module=module.get ('uri'))  
+                 for module in server_modules ] 
+        pprinttable ( rows)
 
 
-
+#taken from
+#http://stackoverflow.com/questions/5909873/python-pretty-printing-ascii-tables
+def pprinttable(rows):
+  if len(rows) > 1:
+    headers = rows[0]._fields
+    lens = []
+    for i in range(len(rows[0])):
+      lens.append(len(max([x[i] for x in rows] + [headers[i]],key=lambda x:len(str(x)))))
+    formats = []
+    hformats = []
+    for i in range(len(rows[0])):
+      if isinstance(rows[0][i], int):
+        formats.append("%%%dd" % lens[i])
+      else:
+        formats.append("%%-%ds" % lens[i])
+      hformats.append("%%-%ds" % lens[i])
+    pattern = " | ".join(formats)
+    hpattern = " | ".join(hformats)
+    separator = "-+-".join(['-' * n for n in lens])
+    print hpattern % tuple(headers)
+    print separator
+    for line in rows:
+      print pattern % tuple(line)
+  elif len(rows) == 1:
+    row = rows[0]
+    hwidth = len(max(row._fields,key=lambda x: len(x)))
+    for i in range(len(row)):
+      print "%*s = %s" % (hwidth,row._fields[i],row[i])
