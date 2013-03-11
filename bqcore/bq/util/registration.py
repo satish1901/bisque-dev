219b383a631007a1c98b5b4cf494b97cbc40cea0
Kristian Kvilekval <kris@cs.ucsb.edu>
2013-03-11 15:52:36 -0700

    invalidate /ds/user on new user also.

--- bqcore/bq/util/registration.py
+++ bqcore/bq/util/registration.py
@@ -3,8 +3,7 @@
 from bq import data_service
 
 def registration_cb (action, user=None):
-    if action == 'update_user':
-        data_service.cache_invalidate('/data_service/user')
+    data_service.cache_invalidate('/data_service/user')
         
 
         
