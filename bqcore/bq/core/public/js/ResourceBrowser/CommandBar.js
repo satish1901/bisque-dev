f596173d0a33941d559911a0401cc3d463de3f22
Utkarsh Gaur <utkarsh.gaur@gmail.com>
2013-05-03 10:22:40 -0700

    Added TreeOrganizer, improved TemplateManager's save mechanism

--- bqcore/bq/core/public/js/ResourceBrowser/CommandBar.js
+++ bqcore/bq/core/public/js/ResourceBrowser/CommandBar.js
@@ -394,7 +394,7 @@ Ext.define('Bisque.ResourceBrowser.CommandBar',
         this.westPanel.doComponentLayout(null, null, true);
 	},
 
-	btnOrganizerClick : function(reload)
+	btnOrganizerClick1 : function(reload)
 	{
         this.westPanel.removeAll(false);
         
@@ -412,7 +412,7 @@ Ext.define('Bisque.ResourceBrowser.CommandBar',
         this.westPanel.doComponentLayout(null, null, true);
 	},
 
-    btnOrganizerClick1 : function(reload)
+    btnOrganizerClick2 : function(reload)
     {
         this.westPanel.removeAll(false);
         
@@ -437,6 +437,31 @@ Ext.define('Bisque.ResourceBrowser.CommandBar',
         this.westPanel.add(this.organizerCt);
     },
     
+    btnOrganizerClick : function(reload)
+    {
+        this.westPanel.removeAll(false);
+
+        this.organizerCt = (reload?undefined:this.organizerCt) || Ext.create('BQ.Organizer.Tree', 
+        {
+            width       :   420,
+            listeners   :   {
+                                scope           :   this,
+                                'QUERY_CHANGED' :   function(uri) { this.msgBus.fireEvent('Browser_ReloadData', uri) }   
+                            },
+                        
+            // state variables
+            config      :   {
+                                tagList         :   ['habitat', 'Plant Structure', 'Genus', 'species'],
+                                resourceType    :   'image',
+                                resourceServer  :   'data_service',
+                                includePublic   :   this.browser.browserParams.wpublic,
+                            }
+        });
+        
+        this.westPanel.setWidth(this.organizerCt.width).show().expand();
+        this.westPanel.add(this.organizerCt);
+    },
+
 	btnLayoutClick : function(item)
 	{
 		switch (item.itemId)
