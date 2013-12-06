01b08e408c40c8a1193652cc1051c0887b14f6bb
Kristian Kvilekval <kris@cs.ucsb.edu>
2013-12-06 10:14:54 -0800

    push module_setup/ensure_binary

--- bqcore/bq/setup/module_setup.py
+++ bqcore/bq/setup/module_setup.py
@@ -170,12 +170,12 @@ def python_setup(scripts,  package_scripts =True, dependency_dir = 'pydist', par
             print ("Could not create python launcher script %s" % e)
 
 
-#def ensure_binary(exe):
-#    'make sure executable is available to the module'
-#    import distutils.spawn
-#    p = distutils.spawn.find_executable(exe)
-#    if p is None:
-#        raise SetupError("Executable required but not found: %s" % exe)
+def ensure_binary(exe):
+    'make sure executable is available to the module'
+    import distutils.spawn
+    p = distutils.spawn.find_executable(exe)
+    if p is None:
+        raise SetupError("Executable required but not found: %s" % exe)
 
 def require(expression, params, throws = True):
     """Require everying in expression
