edc552a746dd28319313d9064c9d34f5b60b20d2
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2014-07-31 20:13:04 +0100

    ui viewer updates

--- bqcore/bq/core/public/js/viewer/imgslicer.js
+++ bqcore/bq/core/public/js/viewer/imgslicer.js
@@ -8,7 +8,7 @@ function ImgSlicer (viewer, name){
     // default values for projection are: '', 'projectmax', 'projectmin'
     // only in the case of 5D image: 'projectmaxt', 'projectmint', 'projectmaxz', 'projectminz'
     this.default_projection  = p.projection || '';
-    this.plane_buffer_sz = 50;  // number of tiles to cache in both z and t
+    this.plane_buffer_sz = 80;  // number of tiles to cache in both z and t
     this.cache_tile_delay_ms = 10; // Delay before requesting a specific tile
     this.update_delay_ms = 250; // Delay before requesting new frames
     this.cache_delay_ms = 700;  // Delay before pre-caching new frames
