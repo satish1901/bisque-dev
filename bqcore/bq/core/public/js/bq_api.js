8eaeeed01cf0d09bc55b5fc8dc1fbfca8f5d6782
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2014-08-23 00:41:22 +0100

    merge fixes

--- bqcore/bq/core/public/js/bq_api.js
+++ bqcore/bq/core/public/js/bq_api.js
@@ -1332,8 +1332,6 @@ BQGObject.prototype.initializeXml = function (node) {
     var t = BQ.util.xpath_node(node, 'tag[@name="color"]');
     if (t)
         this.color_override = t.getAttribute('value').replace('#', '');
-    if (t)
-        this.color_override = t.getAttribute('value').replace('#', '');
 };
 
 BQGObject.prototype.setParent = function (p) {
