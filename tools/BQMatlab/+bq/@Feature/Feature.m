bd44bb6d809db29e8bb95c894c8b8c0dc1e0ec05
Chris Wheat <bobbyjones1989@gmail.com>
2014-09-05 01:59:37 -0700

    changed fetch to static in feature for matlab api

--- tools/BQMatlab/+bq/@Feature/Feature.m
+++ tools/BQMatlab/+bq/@Feature/Feature.m
@@ -1,96 +1,60 @@
-% bq.Image
-% A class wrapping a Bisque image along with it's pixels
-%   Constructor:
-%       Feature(resource, name, user, password)
-%         resource      - URL string or DOM document
-%         name     - Feature name
-%         user     - optional: string
-%         password - optional: string
+% bq.Feature
 %   
 %   AUTHOR:
 %       Chris Wheat
 %
 %   VERSION:
-%       0.1 -  2014-09-03 First implementation
-%
+%       0.1   -  2014-09-03 First implementation
+%       0.1.1 -  2014-09-04 Changed fetch to a static
 classdef Feature < handle
     
-    properties 
-        info = [];
-        resource_list = [];
-        user = '';
-        password = '';
-        name = '';
-        root = '';
-    end % properties
-    
-    methods
+    methods (Static)
 
-        % doc      - URL string or DOM document
-        % element  - optional: DOM element
-        % user     - optional: string
-        % password - optional: string
-        function [self] = Feature(name, resource, user, password, root)
-            self.resource_list = [];
-            if exist('resource', 'var'), self.resource_list = [self.resource_list,resource]; end
-            if exist('name', 'var'), self.name = name; end
-            if exist('user', 'var'), self.user = user; end
-            if exist('root', 'var'), self.root = root; end
-            if exist('password', 'var'), self.password = password; end
-            %self = self@bq.File(supargs{:});
-            %self.init();
-        end % constructor
-
-%         function init(self)
-%             if isempty(self.doc) || ~self.hasAttribute('uri'),
-%                 return;
-%             end
-%             
-%             uri = self.getAttribute('uri');
-%             if ~isempty(self.user) && ~isempty(self.password),
-%                 self.info = bq.iminfo(uri, self.user, self.password);
-%             else
-%                 self.info = bq.iminfo(uri);                
-%             end
-%             self.pixels_url = bq.Url(self.info.pixles_url);            
-%         end % init
-        
-        function self = addResource(self, resource)
-            if exist('resource', 'var'), self.resource_list = [self.resource_list,resource]; end
-        end
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Features
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
-        
-        % filename - optional: if given and not empty represents a filename to save the HDF5 file into
-        %                      if given but is empty instructs to use resource's name
-        % im - the actual image matrix data if no filename was provided or 
-        %      the filename where the image was stored        
-        function [output, info] = fetch(self, filename)
+        % resource_list  - Cell Array 
+        % structure:
+        %       {{'resource type1','resource url1';'resource type2','resource url2'...};...}}
+        % name           - string
+        % user           - optional: string
+        % password       - optional: string
+        function [output, info] = fetch(resource_list, name, root_url, user, password, filename)
+            
+            if ~exist('resource_list', 'var'),
+                error('bq.Feature.fetch:NotEnoughArgs', 'Feature request requires resource list');
+            end
+            
+            if ~exist('name', 'var'),
+                error('bq.Feature.fetch:NotEnoughArgs', 'Feature request requires feature name');
+            end
+            
+            if ~exist('root_url', 'var'),
+                error('bq.Feature.fetch:RequireRootUrl', 'Feature request requires root url');
+            end
+            
+            if ~exist('user', 'var') || isempty(user) || ~exist('password', 'var') || isempty(password),
+                error('bq.Feature.fetch:UserCredentialsInvalid', 'Feature request requires user name and password');
+            end
+            
+            %create feature request body
             body = bq.Factory.new('resource');
-            for i = 1:length(self.resource_list)
+            for i = 1:length(resource_list)
                 f = body.doc.createElement('feature');
                 body.element.appendChild(f); 
-                resource_names = fieldnames(self.resource_list(i));
-                for j = 1:length(resource_names)
-                    f.setAttribute(resource_names(j),self.resource_list(i).(char(resource_names(j))))
+                resource = resource_list{i};
+                for j = 1:size(resource,1)
+                    f.setAttribute(resource{j,1},resource{j,2})
                 end
             end
 
-            source_url = [self.root '/features/' self.name '/hdf'];
-            
+            source_url = [root_url '/features/' name '/hdf'];
             supargs{1} = 'POST';
-            supargs{2} = source_url;  
+            supargs{2} = source_url;
             
             if exist('filename', 'var'), supargs{3} = filename; end %save resulting hdf5 file
  
-            
             supargs{4} = body;
-            if ~isempty(self.user) && ~isempty(self.password)
-                supargs{5} = self.user; 
-                supargs{6} = self.password;                 
+            if ~isempty(user) && ~isempty(password)
+                supargs{5} = user; 
+                supargs{6} = password;                 
             end
             if exist('filename', 'var')
                 [output, info] = bq.connect(supargs{:});
--- tools/BQMatlab/test_feature.m
+++ tools/BQMatlab/test_feature.m
@@ -1,43 +1,101 @@
-url = 'http://128.111.185.26:8080';
+root_url = 'http://128.111.185.26:8080';
 user = 'admin';
 pass = 'admin';
-root = 'http://128.111.185.26:8080';
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% using bq.Feature (Simple Case) 
+% Using bq.Feature 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %Provide the feature name and add a list of resources that the user wants
 %to extract. If a file name is provided on fetch an hdf5 file acting as the
 %response will be saved to the location provided else a Matlab hdf5 object 
 %will be provided.
-feature = bq.Feature('EHD', [], user, pass, root);
-image_url = 'http://128.111.185.26:8080/image_service/image/00-7rEHGCmefZpdLW3porWsDE';
-r = struct('image',image_url);
-feature.addResource(r);
-image_url = 'http://128.111.185.26:8080/image_service/image/00-JSrYb7cH8Y2EX28DX9PoF3';
-r = struct('image',image_url);
-feature.addResource(r);
 
-%Saves the response to disk
-location = feature.fetch('hdf5file.h5');
-
-%returns hdf5 matlab struct
-[response,info] = feature.fetch();
-
-% response = 
+resource_list = {{'image','http://128.111.185.26:8080/image_service/image/00-JSrYb7cH8Y2EX28DX9PoF3'};
+                 {'image','http://128.111.185.26:8080/image_service/image/00-7rEHGCmefZpdLW3porWsDE'}};
+m = bq.Feature.fetch(resource_list, 'EHD', root_url, user, pass);
+% m = 
 % 
 %            image: [2000x2 char]
 %     feature_type: [20x2 char]
 %          feature: [80x2 single]
+%
+
+%f = m.feature(:,1)';
+% f =
+%  0.3182    0.5455         0         0    0.1364    0.6753    0.0649 ...
+
+
 
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Storing images on bisque and then extracting features
+% Saves the response to disk
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The set up is pretty much the same except one can provide a file name and
+% the response will be save to disk at the location of the provided file
+filename = 'hdf5file.h5';
+bq.Feature.fetch(resource_list, 'EHD', root_url, user, pass, filename);
+
+%displaying the hdf5 file 
+h5disp('hdf5file.h5','/values');
 %
+% HDF5 hdf5file.h5 
+% Dataset 'values' 
+%     Size:  2
+%     MaxSize:  Inf
+%     Datatype:   H5T_COMPOUND
+%         Member 'image':  H5T_STRING
+%             String Length: 2000
+%             Padding: H5T_STR_NULLTERM
+%             Character Set: H5T_CSET_ASCII
+%             Character Type: H5T_C_S1
+%         Member 'feature_type':  H5T_STRING
+%             String Length: 20
+%             Padding: H5T_STR_NULLTERM
+%             Character Set: H5T_CSET_ASCII
+%             Character Type: H5T_C_S1
+%         Member 'feature':  H5T_ARRAY
+%             Size: 80
+%             Base Type:  H5T_IEEE_F32LE (single)
+%     ChunkSize:  1792
+%     Filters:  none
+%     Attributes:
+%         'CLASS':  'TABLE'
+%         'VERSION':  '2.7'
+%         'TITLE':  ''
+%         'FIELD_0_NAME':  'image'
+%         'FIELD_1_NAME':  'feature_type'
+%         'FIELD_2_NAME':  'feature'
+%         'FIELD_0_FILL':  ''
+%         'FIELD_1_FILL':  ''
+%         'FIELD_2_FILL':  0.000000
+%         'NROWS':  2
+
+%reading from the table
+m = h5read('hdf5file.h5','/values');
+%
+% m = 
+% 
+%            image: [2000x2 char]
+%     feature_type: [20x2 char]
+%          feature: [80x2 single]
 %
 
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Storing images on bisque and then extracting features (unfinished)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Alright lets have some fun. We can upload to bisque and then extract
+% features from the uploaded image
+
+%Here is a very famous image
+I = imread('coins.png'); 
+
+%Lets upload it to bisque
+args = struct('filename', 'coins.png');
+image = bq.Image.store(I, args, root_url, user, pass); 
 
 
 %%
