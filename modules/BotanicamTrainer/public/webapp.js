03aab1aac63c78e536afa7df6b291c8ffaa5ab69
Chris Wheat <bobbyjones1989@gmail.com>
2014-11-12 17:59:02 -0800

    replace botanicam and botanicamtrainer with python versions and moved old to unported

--- modules/BotanicamTrainer/public/webapp.js
+++ modules/BotanicamTrainer/public/webapp.js
@@ -1,182 +1,50 @@
-BQ.selectors.resources.query = 'BQ.selectors.Botanicam.Query';
-BQ.selectors.parameters.tagselector = 'BQ.selectors.Botanicam.TagSelector';
+//BQ.selectors.resources.resource_set = 'BQ.selectors.Botanicam.ResourceSet';
+BQ.selectors.parameters.tag_selector = 'BQ.selectors.Botanicam.TagSelector';
+BQ.renderers.resources.model_viewer = 'BQ.renderers.Botanicam.ModelViewer';
 
-
-// provide our renderer
-Ext.define('BQ.selectors.Botanicam.Query', {
-    alias: 'widget.selectorquery',
-    extend: 'BQ.selectors.Selector',
-    requires: ['Ext.button.Button', 'Bisque.ResourceBrowser.Dialog','Ext.grid.Panel','Ext.data.Store'],
-    
-    //layout: 'auto',
-    height: 100,
-    
-    layout: {
-        type: 'vbox',
-        defaultMargins:{top:0, right:0, bottom:0, left:20},
-        pack  : 'start',
-    },
-    
-    initComponent : function() {
-        var resource = this.resource;
-        var template = resource.template || {};
-        
-        
-        Ext.regModel('QUERY', {
-            fields: [
-                {type: 'string', name: 'Tag'},
-                {type: 'string', name: 'Name'},
-                    ]
-            });
-        
-        var querytags = [];
-        
-        // The data store holding the states
-        var store = Ext.create('Ext.data.Store', {
-            model: 'QUERY',
-            data: querytags
-        });
-            
-        //drawing all the items 
-        this.items = [];
-        this.items.push( {xtype: 'label', text:template.label+':' } );
-        this.items.push( {xtype: 'tbtext', text:'<br/>'} );
-        this.items.push( Ext.create('Ext.button.Button', {
-            id:'query',
-            iconAlign: 'right',
-            width: 150,
-            height: 50,
-            text: 'Select a Query',
-            //iconCls: 'upload',
-            //scale: 'large',
-            //cls: 'x-btn-default-large',
-            tooltip: template.description,
-            handler: Ext.Function.bind( this.selectMex, this ),
-        }));
-        this.items.push( {xtype: 'tbtext', text:'<br/>'} );
-        output = Ext.create('Ext.grid.Panel', {
-            store: store,
-            hidden:true,
-            autoScroll: true,
-            width: 360,
-            height: 125,
-            stateful: true,
-            title: 'Tags Query',
-            stateId: 'stateGrid',
-            enableColumnHide:false,
-            forceFit: true,
-            columns: [
-                {
-                    text     : 'Tags',
-                    width    : 175,
-                    sortable : true,
-                    dataIndex: 'Tag'
-                },
-                {
-                    text     : 'Names',
-                    width    : 175,
-                    sortable : true,
-                    dataIndex: 'Name'
-                },
-            ],
-            viewConfig: {
-                stripeRows: true,
-                //enableTextSelection: true
-            }                   
-        });
-                
-        this.output=output;
-        this.items.push( this.output )  
-        this.callParent();
-    },
-
-    selectMex: function() {
-        var resource = this.resource;
-        var template = resource.template || {};
-        var resourceDialog = Ext.create('Bisque.QueryBrowser.Dialog', {
-            'height'  : '85%',
-            'width'   :  '85%',
-            dataset   : '/data_service/image',
-            query_resource_type: 'image',
-            listeners : {
-                'Select' : this.onselected,
-                scope: this
-            },
-        });
-    },
-
-    onerror: function(message) {
-        BQ.ui.error('Error fetching resource:<br>' + message);
-    },
-
-    onselected: function(browser, query) {
-    
-        this.resource.value = query;
-        
-        //fires event to place tags in combo box
-        this.fireEvent('changed', query);
-        var querytags = [];
-        //parse query to place in the display box
-        d_query=query.replace(/"/g, ""); //removing '"'
-        d_query=d_query.split(' AND ');
-        for(i=0;i<d_query.length;i++){
-            s_query=d_query[i].split(':');
-            querytags.push([s_query[0],s_query[1]]);
-        }
-        
-        //resets value after picking new query
-        this.output.store.removeAll(); //removes all data from the combo box
-        this.output.store.add(querytags); //writes new data to the combo box
-        
-        
-        //set display box to visiable
-        this.setHeight(250,true); //resets size
-        
-        if (this.output.isHidden()){
-            this.output.setVisible(true)
-        };
-        // validate the query selection
-        if (!this.validate()) return;
-    },
-
-    isValid: function() {
-        if (!this.resource.value) {
-            BQ.ui.tip(this.getId(), 'You need to select a query to train!', {anchor:'left',});
-            return false;
-        }
-        return true;
-    },
-    //reloads mex
-    select: function(resource) {
-        this.onselected([],resource.value);
-    },
-    
-});
-
+/*
+ *  The tag selector for Botanicam Trainer Module
+ * 
+ *  Warning: Takes in only dataset and query resources
+ *  
+ * Example in XMl
+ * 
+ *  <tag name="tag_names" type="tag_selector">     
+ *      <template>
+ *          <tag name="label" value="Enter tags to be classified" />
+ *          <tag name="reference" value="resource_url"/>
+ *      </template>  
+ *  </tag>
+ * 
+ * tag_name - variable for the resource returned
+ * tag_selector - the parameter to define the tag selector template
+ * 
+ *  parameters
+ *      label - The text displayed next to the combo box
+ *      reference - choose the variable of the resource selected or the resource one 
+ * wants to reference (resource must be a query or dataset)
+ *      
+ */
 Ext.define('BQ.selectors.Botanicam.TagSelector', {
     alias: 'widget.selectortagselector',
     extend: 'BQ.selectors.Selector',
 
-    require: (['Ext.form.Panel','Ext.form.field.ComboBox','Ext.data.Store']),
+    require: (['Ext.form.field.ComboBox','Ext.data.Store']),
     
-    height: 70,
-    layout: {
-        type: 'vbox',
-        defaultMargins:{top:10, right:0, bottom:0, left:20},
-        pack  : 'start',
-    
-    },
+    height: 30,
+    width: '100%',
+    anchor: '0 0',
+    layout: 'hbox',
 
     initComponent : function() {
+      
         var resource = this.resource;
         var template = resource.template || {};
     
         var reference = this.module.inputs_index[template.reference];
         
         Ext.regModel('State', {
-                fields: [
-                   {type: 'string', name: 'TAGS'},
-                        ]
+                fields: [{type: 'string', name: 'TagValues'}]
             });
         
         // ComboBox with multiple selection enabled
@@ -188,43 +56,53 @@ Ext.define('BQ.selectors.Botanicam.TagSelector', {
             data: states
             });
                   
-        var multiSelectCombo = Ext.create('Ext.form.field.ComboBox', {
-        
-            id:'combo1',
-            fieldLabel: 'Select atleast one tag',
+        this.multiSelectCombo = Ext.create('Ext.form.field.ComboBox', {
+            name: resource.name,
+            labelWidth: 200,
+            //autoWidth: true,
+            labelAlign: 'right',
+            fieldLabel: template.label?template.label:'',
             multiSelect: true,
-            displayField: 'TAGS',
-            width: 600,
-            labelWidth: 130,
+            displayField: 'TagValues',
+            width: '100%',
             store: store,
-            editable : false, 
+            editable : false,
             queryMode: 'local',
+            listeners: {
+                scope: this,
+                change: function() {
+                    this.onselected()
+                }
+            },
             
         });
-        
-        this.multiSelectCombo=multiSelectCombo;
-        multiSelectCombo.on('select', this.onselected, this); //validation
         this.items = [];
-        this.items.push(multiSelectCombo);
+        this.items.push(this.multiSelectCombo);
         
         if (reference && reference.renderer) {
             this.reference = reference.renderer;
             //run parcer and reset the variables
-            this.reference.on( 'changed', this.parse , this );
+            this.reference.on( 'changed', this.fetchTagList , this );
         }
         
         this.callParent();
     },
     
-    parse : function(ref) {
+    fetchTagList : function(me) {
             //fetches tags
-            this.tags=ref;
-            var modtags=ref.replace(/"/g, "%22");
-            var modtags=modtags.replace(/ /g, "%20");
-            this.current_mex_uri='/data_service/image/?tag_names=1&tag_query='+ modtags;
+
+            if (me['resource'].type=='resource'){
+                this.tag_url=me['resource'].value+'&tag_names=1';
+            }
+            else if (me['resource'].type=='dataset') {
+                this.tag_url=me['resource'].value+'/value?tag_names=1';
+            } 
+            else {
+                this.onerror(me['rource'].value)
+            }
             this.setLoading('Fetching memebrs...');
             BQFactory.request({ 
-                uri:     this.current_mex_uri, 
+                uri:     this.tag_url,
                 cb:      callback(this, 'onfetched'),
                 errorcb: callback(this, 'onerror'),
             });
@@ -239,24 +117,26 @@ Ext.define('BQ.selectors.Botanicam.TagSelector', {
         var states = [];
         for ( i=0; i<x.length;i++){
             var y=x[i].name;
-            states.push({"TAGS":y});
+            states.push({"TagValues":y});
             }
         this.multiSelectCombo.setValue(); //resets value after picking new query
-        this.multiSelectCombo.getStore('combo1').removeAll(); //removes all data from the combo box
-        this.multiSelectCombo.getStore('combo1').add(states); //writes new data to the combo box
+        this.multiSelectCombo.store.removeAll(); //removes all data from the combo box
+        this.multiSelectCombo.store.add(states); //writes new data to the combo box
         if(this.newresource){
             this.multiSelectCombo.setValue(this.newresource);
             delete this.newresource;
+            this.onselected();
         }
     },
     
     onerror: function(message) {
-        BQ.ui.error('Error fethnig resource:<br>' + message); 
+        BQ.ui.error('Error fetching resource:<br>' + message); 
     },   
     
     
     onselected : function() {
                 this.resource.value = this.multiSelectCombo.getValue();
+                if (this.resource.value.length<1) {this.resource.value=null} //need to set to null to trigger the IsValid
                 if (!this.validate()) return;
     },
 
@@ -271,8 +151,124 @@ Ext.define('BQ.selectors.Botanicam.TagSelector', {
     select: function(resource) { 
         if(!this.multiSelectCombo.store.data.item){
                 this.newresource=resource.value ; //creates new resource to store till after select query is rendered
-                this.onselected();
+        }
+    },
+    
+});
+
+Ext.define('BQ.renderers.Botanicam.ModelViewer', {
+    alias: 'widget.rendererclassifiermodel',
+    extend: 'BQ.renderers.Renderer',
+    requires: ['Bisque.ResourceTagger'],
+
+    height: 300,
+    layout: {
+        type: 'vbox',
+        align : 'stretch',
+        pack  : 'start',
+    },
+
+    initComponent : function() {
+        var definition = this.definition;
+        var template = definition.template || {};
+        var resource = this.resource;
+        if (!definition || !resource) return;
+        template.label = template.label || 'Output file';
+
+        BQFactory.request( { uri: resource.value,
+                             cb:  callback(this, 'onFile'), });
+    
+
+
+        this.items = [];
+        this.items.push( {xtype: 'label', html:(template.label?template.label:resource.name),  } );
+        this.callParent();
+    },
+
+    onFile : function(r) {
+        this.file = r;
+        this.tagger = Ext.create('Bisque.ResourceTagger', {
+            //resource: resource.type?resource.value:resource, // reference or resource
+            resource: this.file.uri,
+            flex: 1,
+            cls: 'tagger',
+            //title : template.label?template.label:resource.name,
+            viewMode : 'ReadOnly',
+            tbarCfg : {
+                layout: {
+                    pack: 'center'
                 }
-    }, 
+            },  //for center align
+            tbar: [{
+                xtype: 'button',
+                text: 'Open "<b>'+this.file.name+'</b>"',
+                //iconCls: 'folder',
+                scale: 'large',
+                //style:{width:'30%'},
+                width: '30%',
+                //cls: 'x-btn-default-large',
+                //tooltip: 'Download',
+                //handler: Ext.Function.bind( this.download, this ),
+                listeners: {
+                    click: function(me, resource) {
+                        window.open(bq.url('/client_service/view?resource='+this.file.uri));
+                    },
+                    scope: this
+                },
+            },{
+                xtype: 'button',
+                text: 'Download "<b>'+this.file.name+'</b>"',
+                //iconCls: 'download',
+                scale: 'large',
+                width: '30%',
+                //cls: 'x-btn-default-large',
+                //tooltip: 'Download',
+                //handler: Ext.Function.bind( this.download, this ),
+                listeners: { 
+                    click: function(me, resource) {
+                        window.open(this.file.src);
+                    },
+                    scope: this
+                },
+            }]
+        });
+        //this.items.push(this.tagger);
+        this.add(this.tagger);
+        /*
+        this.add({
+            xtype: 'button',
+            text: 'Open "<b>'+this.file.name+'</b>"',
+            iconCls: 'folder',
+            //scale: 'large',
+            //style:{width:'30%'},
+            width: '30%',
+            //cls: 'x-btn-default-large',
+            //tooltip: 'Download',
+            //handler: Ext.Function.bind( this.download, this ),
+            listeners: { 
+                click: function(me, resource) {
+                    window.open(bq.url('/client_service/view?resource='+this.file.uri));
+                },
+                scope: this
+            },
+        });
+        this.add({
+            xtype: 'button',
+            text: 'Download "<b>'+this.file.name+'</b>"',
+            iconCls: 'download',
+            scale: 'large',
+            width: '30%',
+            //cls: 'x-btn-default-large',
+            //tooltip: 'Download',
+            //handler: Ext.Function.bind( this.download, this ),
+            listeners: { 
+                click: function(me, resource) {
+                    window.open(this.file.src);
+                },
+                scope: this
+            },
+        });*/
+    },    
+
 });
 
--- modules/BotanicamTrainer/setup.py
+++ modules/BotanicamTrainer/setup.py
@@ -1,15 +1,12 @@
 # Install script for BotanicamTrainer
 import sys
-from bq.setup.module_setup import matlab_setup, read_config, ensure_binary
+from bq.setup.module_setup import python_setup, read_config
 
-def setup(params, *args, **kw):
-    ensure_binary('svm-predict')
-    ensure_binary('svm-scale')       
-    return matlab_setup('BotanicamTrainer', params=params)
+def setup(params, *args, **kw):    
+    return python_setup('BotanicamTrainerFS.py', params=params)
     
 if __name__ =="__main__":
     params = read_config('runtime-bisque.cfg')
     if len(sys.argv)>1:
         params = eval (sys.argv[1])
-
     sys.exit(setup(params))
