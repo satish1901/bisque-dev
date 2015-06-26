7675510d0b280e03eefe73c79eb8e6a2b7a417c8
Kristian Kvilekval <kris@cs.ucsb.edu>
2015-06-26 11:05:36 -0700

    catch driver exceptions

--- bqserver/bq/blob_service/controllers/mount_service.py
+++ bqserver/bq/blob_service/controllers/mount_service.py
@@ -575,6 +575,9 @@ class MountServer(TGController):
             except IllegalOperation, e:
                 log.debug ("failed %s store on %s with %s", storepath, store_name, e )
                 storeurl = lpath = None
+            except:
+                log.exception ("failed %s store on %s with %s", storepath, store_name, e )
+                storeurl = lpath = None
         log.debug('store_blob: %s %s %s', storeurl, lpath, etree.tostring(resource))
         if storeurl is None:
             log.error ('storing %s failed (%s)', storepath, storeurl)
