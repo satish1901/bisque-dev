b20b823dcc58a230b9635470249b40e468c80840
Kristian Kvilekval <kris@cs.ucsb.edu>
2015-07-22 12:00:16 -0700

    disable value column migration

--- migrations/versions/14bcc365e5d6_value_columns.py
+++ migrations/versions/14bcc365e5d6_value_columns.py
@@ -10,6 +10,7 @@ Create Date: 2014-12-18 15:17:23.373858
 revision = '14bcc365e5d6'
 down_revision = '1fd412bca00e'
 
+import sys
 from alembic import op
 from alembic import context
 import sqlalchemy as sa
@@ -23,9 +24,11 @@ from bq.util.bisquik2db import parse_bisque_uri
 from bq.util.hash import is_uniq_code
 
 def upgrade():
+
+    return
+
     op.add_column('taggable', Column('resource_number', Float))
     op.add_column('taggable', Column('resource_obj',  Integer, ForeignKey ('taggable.id')))
-
     cntxt = context.get_context()
     SessionMaker = sessionmaker(bind=cntxt.bind)
 
@@ -38,29 +41,28 @@ def upgrade():
     converted_numbers = 0
     converted_objs = 0
     rows = 0
-    for tv in DBSession.query(Taggable).filter(Taggable.resource_value != None):
+    for tv in DBSession.query(Taggable).filter(Taggable.resource_value != None).yield_per(10000):
         try:
             tv.resource_number  = float(tv.resource_value)
             converted_numbers += 1
-            #print ",number ",
-            continue
+            #print >>sys.stderr, ",n "
         except ValueError:
-            pass
+            try:
+                service, clname, ida, rest = parse_bisque_uri(tv.resource_value)
+                if ida and is_uniq_code (ida):
+                    #log.debug("loading resource_uniq %s" % ida)
+                    resource_link = DBSession.query(Taggable).filter_by(resource_uniq = ida).first()
+                    if resource_link:
+                        tv.resource_obj = resource_link.id
+                        #print >>sys.stderr, ",l:", resource_link.id
+                        converted_objs += 1
+            except IndexError:
+                pass
 
-        try:
-            service, clname, ida, rest = parse_bisque_uri(tv.resource_value)
-            if ida and is_uniq_code (ida):
-                #log.debug("loading resource_uniq %s" % ida)
-                resource_link = DBSession.query(Taggable).filter_by(resource_uniq = ida).first()
-                if resource_link:
-                    tv.resource_obj = resource_link.id
-                    #print ",linked:", resource_link.id,
-                    converted_objs += 1
-        except IndexError:
-            pass
         rows += 1
 	if rows  % 1000 == 0:
-            DBSession.flush()
+            DBSession.commit()
+            print >>sys.stderr, ".",
 
     print "converted %s numbers and %s objects" % (converted_numbers, converted_objs)
     DBSession.commit()
@@ -68,6 +70,8 @@ def upgrade():
 
 
 def downgrade():
+    return
+
     op.drop_column('taggable', 'resource_number')
     op.drop_column('taggable', 'resource_obj')
 
