dd8fd42bca50551859f0852ced9c31412c20e5bc
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2015-11-18 18:11:33 +0000

    table service: added multi-table UI support

--- bqcore/bq/core/public/plugins/table/table.js
+++ bqcore/bq/core/public/plugins/table/table.js
@@ -1,9 +1,20 @@
 /*******************************************************************************
 Author: Dmitry Fedorov
 Copyright: 2015 (C) Center for Bio-Image Informatics, University of California at Santa Barbara
+
+Exportable classes:
+    BQ.table.Panel - renders the table resource
+    BQ.table.View - renders a given sub-table of a table resource
+
+Browser components:
+    Bisque.Resource.Table.Page
+    ...
+    Bisque.Resource.Table.Grid
+
 *******************************************************************************/
 
 Ext.namespace('BQ.data');
+Ext.namespace('BQ.table');
 BQ.data.pageSize = 500;
 
 Ext.require([
@@ -12,6 +23,10 @@ Ext.require([
     'Ext.util.*',
     'Ext.grid.plugin.BufferedRenderer'
 ]);
+
+//--------------------------------------------------------------------------------------
+// Table json proxy
+//--------------------------------------------------------------------------------------
 
 Ext.define('BQ.data.proxy.Table', {
     extend: 'Ext.data.proxy.Ajax',
@@ -53,6 +68,10 @@ Ext.define('BQ.data.proxy.Table', {
 
 });
 
+//--------------------------------------------------------------------------------------
+// Table json reader
+//--------------------------------------------------------------------------------------
+
 Ext.define('BQ.data.reader.Table', {
     extend: 'Ext.data.reader.Json',
     alternateClassName: 'BQ.data.TableReader',
@@ -72,6 +91,227 @@ Ext.define('BQ.data.reader.Table', {
         return me.callParent([data]);
     },
 });
+
+//--------------------------------------------------------------------------------------
+// Table grid view - renders actual grid content for one given table
+// required parameters:
+//     resource - the table resource
+//     path - sub table path
+//--------------------------------------------------------------------------------------
+
+BQ.table.encodeURIpath = function(p) {
+    p = encodeURIComponent(p).replace('%2F', '/');
+    if (p[0] == '/') return p; else return '/'+p;
+}
+
+Ext.define('BQ.table.View', {
+    extend: 'Ext.container.Container',
+    alias: 'widget.bq_table_view',
+    cls: 'bq_table_view',
+    layout: 'fit',
+
+    initComponent : function() {
+        // temporary columns and store needed to init empty gridpanel without errors
+        this.store = Ext.create('Ext.data.Store', {
+            fields:['name'],
+            data:{ 'items': [{ 'name': '' }, ]},
+            proxy: {
+                type: 'memory',
+                reader: {
+                    type: 'json',
+                    root: 'items'
+                }
+            }
+        });
+
+        // create view components: grid and tagger
+        this.items = [{
+            xtype: 'gridpanel',
+            itemId  : 'table',
+            autoScroll: true,
+            border: 0,
+            viewConfig: {
+                stripeRows: true,
+                //forceFit: true
+            },
+            plugins: 'bufferedrenderer',
+            store: this.store,
+            columns: [
+                { text: '',  dataIndex: 'name' },
+            ],
+        }];
+
+        this.callParent();
+    },
+
+    afterRender : function() {
+        this.callParent();
+        if (this.info) {
+            this.onTableInfo();
+            return;
+        }
+
+        // load table info to configure columns and the store
+        //this.setLoading('Fetching table info...');
+        Ext.Ajax.request({
+            url: '/table/' + this.resource.resource_uniq + BQ.table.encodeURIpath(this.path)+ '/info/format:json',
+            callback: function(opts, succsess, response) {
+                if (response.status>=400 || !succsess)
+                    BQ.ui.error(response.responseText);
+                else
+                    this.onTableInfo(response.responseText);
+            },
+            disableCaching: false,
+            scope: this,
+            listeners: {
+                scope: this,
+                beforerequest   : function() { this.setLoading('Loading table...'); },
+                requestcomplete : function() { this.setLoading(false); },
+                requestexception: function() { this.setLoading(false); },
+            },
+        });
+    },
+
+    onTableInfo: function(txt) {
+        //this.setLoading(false);
+        var json = this.info,
+            h = null;
+        if (!this.info && txt) {
+            json = Ext.JSON.decode(txt);
+        }
+        this.fields = [];
+        this.columns = [];
+        for (var i=0; i<json.headers.length; ++i) {
+            var h = json.headers[i],
+                t = json.types[i],
+                tt = 'string';
+
+            if (t.indexOf('int')>=0)
+                tt = 'int';
+            else if (t.indexOf('float')>=0)
+                tt = 'float';
+
+            this.fields.push({
+                name: i, // need to use numbers for reading from json array
+                type: tt,
+                useNull: true,
+            });
+
+            this.columns.push({
+                text: h,
+                //flex: 1,
+                dataIndex: i, // need to use numbers for reading from json array
+                //sortable: true,
+            });
+        }
+
+        this.store = Ext.create('Ext.data.Store', {
+            autoLoad: true,
+            autoSync: true,
+            fields: this.fields,
+
+            buffered: true,
+            leadingBufferZone: 100, // 300
+            pageSize: BQ.data.pageSize,
+
+            proxy: {
+                //type: 'rest',
+                type: 'bq-table',
+                url: '/table/' + this.resource.resource_uniq, // + '/format:json',
+                //noCache : false,
+                //pageParam: undefined,
+                //startParam: undefined,
+                //limitParam: undefined,
+                reader: {
+                    type: 'bq-table', // 'json',
+                    root: 'table'
+                },
+                //writer: {
+                //    type: 'json'
+                //}
+            },
+        });
+        var table = this.queryById('table');
+        table.reconfigure(this.store, this.columns);
+    },
+
+});
+
+
+//--------------------------------------------------------------------------------------
+// Table viewer - renders all tables present in the table resource
+// required parameters:
+//     resource - the table resource
+//--------------------------------------------------------------------------------------
+
+Ext.define('BQ.table.Panel', {
+    extend: 'Ext.tab.Panel',
+    alias: 'widget.bq_table_panel',
+    cls: 'bq_table_panel',
+    deferredRender: true,
+    activeTab : 0,
+    plain : true,
+
+    initComponent : function() {
+        this.callParent();
+    },
+
+    afterRender : function() {
+        this.callParent();
+        // load table info to configure columns and the store
+        //this.setLoading('Fetching table info...');
+        Ext.Ajax.request({
+            url: '/table/' + this.resource.resource_uniq + '/info/format:json',
+            callback: function(opts, succsess, response) {
+                if (response.status>=400 || !succsess)
+                    BQ.ui.error(response.responseText);
+                else
+                    this.onTableInfo(response.responseText);
+            },
+            disableCaching: false,
+            scope: this,
+            listeners: {
+                scope: this,
+                beforerequest   : function() { this.setLoading('Loading table...'); },
+                requestcomplete : function() { this.setLoading(false); },
+                requestexception: function() { this.setLoading(false); },
+            },
+        });
+    },
+
+    onTableInfo: function(txt) {
+        this.setLoading(false);
+        var json = Ext.JSON.decode(txt);
+        this.tables = json.tables;
+        if (this.tables && this.tables.length>1) {
+            var p=undefined,
+                i=undefined;
+            for (i=0; (p=this.tables[i]); ++i) {
+                this.add({
+                    xtype: 'bq_table_view',
+                    border: 0,
+                    resource: this.resource,
+                    path: p,
+                    title: p,
+                });
+            }
+        } else {
+            this.add({
+                xtype: 'bq_table_view',
+                border: 0,
+                resource: this.resource,
+                path: p,
+                info: json,
+            });
+            this.getTabBar().hide();
+        }
+    },
+
+});
+
+//--------------------------------------------------------------------------------------
+// Resource renderer
+//--------------------------------------------------------------------------------------
 
 Ext.define('Bisque.Resource.Table.Page', {
     extend : 'Bisque.Resource.Page',
@@ -131,134 +371,16 @@ Ext.define('Bisque.Resource.Table.Page', {
                     title : 'Annotations',
                 }]
             }, {
-                xtype: 'gridpanel',
-                itemId  : 'table',
-                autoScroll: true,
+                xtype: 'bq_table_panel',
                 flex: 2,
                 region : 'center',
                 border: 0,
-                viewConfig: {
-                    stripeRows: true,
-                    //forceFit: true
-                },
-                plugins: 'bufferedrenderer',
-                store: this.store,
-                columns: this.columns,
+                resource: this.resource,
+                //path: '',
             }],
         });
         //this.toolbar.doLayout();
-
-        // load table info to configure columns and the store
-        this.setLoading(true);
-        Ext.Ajax.request({
-            url: '/table/' + this.resource.resource_uniq + '/info/format:json',
-            callback: function(opts, succsess, response) {
-                if (response.status>=400 || !succsess)
-                    BQ.ui.error(response.responseText);
-                else
-                    this.onTableInfo(response.responseText);
-            },
-            scope: this,
-            disableCaching: false,
-            listeners: {
-                scope: this,
-                beforerequest   : function() { this.setLoading('Loading table...'); },
-                requestcomplete : function() { this.setLoading(false); },
-                requestexception: function() { this.setLoading(false); },
-            },
-        });
-
     },
-
-    onTableInfo: function(txt) {
-        this.setLoading(false);
-        var json = Ext.JSON.decode(txt),
-            h = null;
-
-        this.info = json;
-        this.fields = [];
-        this.columns = [];
-        for (var i=0; i<json.headers.length; ++i) {
-            var h = json.headers[i],
-                t = json.types[i],
-                tt = 'string';
-
-            if (t.indexOf('int')>=0)
-                tt = 'int';
-            else if (t.indexOf('float')>=0)
-                tt = 'float';
-
-            this.fields.push({
-                name: i, // need to use numbers for reading from json array
-                type: tt,
-                useNull: true,
-            });
-
-            this.columns.push({
-                text: h,
-                //flex: 1,
-                dataIndex: i, // need to use numbers for reading from json array
-                //sortable: true,
-            });
-        }
-
-        this.store = Ext.create('Ext.data.Store', {
-            autoLoad: true,
-            autoSync: true,
-            fields: this.fields,
-
-            buffered: true,
-            leadingBufferZone: 100, // 300
-            pageSize: BQ.data.pageSize,
-
-            proxy: {
-                //type: 'rest',
-                type: 'bq-table',
-                url: '/table/' + this.resource.resource_uniq, // + '/format:json',
-                //noCache : false,
-                //pageParam: undefined,
-                //startParam: undefined,
-                //limitParam: undefined,
-                reader: {
-                    type: 'bq-table', // 'json',
-                    root: 'table'
-                },
-                //writer: {
-                //    type: 'json'
-                //}
-            },
-        });
-        var table = this.queryById('table');
-        table.reconfigure(this.store, this.columns);
-    },
-
-    onTableContents: function(txt) {
-        var json = Ext.JSON.decode(txt);
-        this.setLoading(false);
-        this.table = json;
-        //this.ontableLoaded(this.table);
-    },
-
-    onLoad: function(cb, url, r) {
-        this.setLoading(false);
-        if (r && r.target && r.target.responseText) {
-            cb(r.target.responseText);
-        } else {
-            BQ.ui.error('Could not fetch '+url);
-        }
-    },
-
-    onError: function(r) {
-        this.setLoading(false);
-        var m = r.target ? r.target.responseText : undefined;
-        if (!m && r.target.status === 0) {
-            m = 'Request failed probably due to Access-Control-Allow-Origin protection in your browser';
-        } else if (!m && r.target.status>0) {
-            m = 'Request failed';
-        }
-        BQ.ui.error(m);
-    },
-
 });
 
 Ext.define('Bisque.Resource.Table.Compact', {
