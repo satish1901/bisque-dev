7785b62bd3a61b9f7acf92c543919ed582249ac6
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2016-01-20 05:26:33 +0000

    added shares and queries

--- tools/BQMatlab/test_mex.m
+++ tools/BQMatlab/test_mex.m
@@ -4,6 +4,18 @@ pass = 'password';
 module_url = '/module_service/MyData'
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% running a module using a higher level API
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+mex = bq.Factory.new('mex');
+inputs = mex.addTag('inputs'); 
+inputs.addTag('mex_url', '', 'system-input'); 
+inputs.addTag('bisque_token', '', 'system-input'); 
+inputs.addTag('image_url', 'http://bisque_root/some_image'); 
+mex.save([url module_url '/execute'], user, pass);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% running a module using lower level post
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -18,15 +30,4 @@ mex = [input sprintf('</mex>\n')];
 
 bq.post([url module_url '/execute'], mex, user, pass);
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% running a module using a higher level API
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-mex = bq.Factory.new('mex');
-inputs = mex.addTag('inputs'); 
-inputs.addTag('mex_url', '', 'system-input'); 
-inputs.addTag('bisque_token', '', 'system-input'); 
-inputs.addTag('image_url', 'http://bisque_root/some_image'); 
-mex.save([url module_url '/execute'], user, pass);
 
