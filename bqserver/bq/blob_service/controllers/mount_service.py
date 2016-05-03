d593573666d22eb66cc30183bdb3b648bcaeef4d
Kristian Kvilekval <kris@cs.ucsb.edu>
2016-05-03 13:40:27 -0700

    fix blob_service/store paths

--- bqserver/bq/blob_service/controllers/mount_service.py
+++ bqserver/bq/blob_service/controllers/mount_service.py
@@ -260,6 +260,10 @@ class MountServer(TGController):
             #resp.sort (key = lambda x: x.get ('name'))
             q = resp
 
+        fullpath = ['', 'blob_service', 'store', store_name]
+        fullpath.extend (path)
+        self.mapuris (q, top="/".join (fullpath))
+
         return etree.tostring(q)
 
     def _post(self, path, **kw):
@@ -300,14 +304,26 @@ class MountServer(TGController):
         root = data_service.query('store', resource_unid='(root)', view='full')
         #root = data_service.query('store', resource_unid='(root)', view='short')
         if len(root) == 0:
-            return  self._create_default_mounts()
+            found_root=  self._create_default_mounts()
         if len(root) == 1:
-            return self._create_default_mounts(root[0])
+            found_root =  self._create_default_mounts(root[0])
         elif len(root) > 1:
             log.error("Root store created more than once: %s ", etree.tostring(root))
             return None
 
-        return root[0]
+        self.mapuris(found_root)
+        return found_root
+
+    def mapuris (self, store_resource, top="/blob_service/store"):
+        log.debug ("STORE RESOURCE -> %s", etree.tostring (store_resource))
+        store_resource.set ('uri', top)
+        resource_iter  = store_resource.iter()
+        next (resource_iter)
+        for el in resource_iter:
+            if el.tag in ('link', 'dir'):
+                el.set ('uri', (el.getparent() and el.getparent().get('uri', 'ROOT') or '') + '/' + el.get ('resource_unid', 'UNID'))
+
+
 
     def _create_default_mounts(self, root=None):
         'translate system stores into mount (store) resources'
@@ -606,7 +622,8 @@ class MountServer(TGController):
 
                 # Store URL may be changed during driver push by disambiguation code.
                 # revert change from changeset:2297 to always use storeurl
-                name = os.path.basename(resource_name) or tounicode(url2localpath(os.path.basename(storeurl)))
+                #name = os.path.basename(resource_name) or tounicode(url2localpath(os.path.basename(storeurl)))
+                name = tounicode(url2localpath(os.path.basename(storeurl)))
                 resource.set('name', join_subpath(name, sub))
                 resource.set('value', join_subpath(storeurl, sub))
                 log.debug('_save_store: %s', etree.tostring(resource))
