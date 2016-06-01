fb3929651d6ca2879caabb29f6eb8a7f1944baf1
Kristian Kvilekval <kris@cs.ucsb.edu>
2016-05-31 23:47:09 -0700

    remove tgsched from setup.py .. breaks build

--- bqserver/setup.py
+++ bqserver/setup.py
@@ -11,7 +11,7 @@ install_requires = [
         "gdata",
         "Turbomail",
         "genshi",
-        "TGScheduler",
+#        "TGScheduler",
         "boto",
         "numpy",
         "ordereddict",
