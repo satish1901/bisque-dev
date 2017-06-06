c51e4fd62e2e62e9cf7df151db1052a3792ab6f2
Kristian Kvilekval <kris@cs.ucsb.edu>
2017-06-06 13:59:06 -0700

    use mv instead of rsynv in docker_run

--- bqengine/bq/engine/controllers/docker_env.py
+++ bqengine/bq/engine/controllers/docker_env.py
@@ -24,8 +24,8 @@ docker wait  $CONTAINER
 ${DOCKER_OUTPUTS}
 # docker will not copy to existing directory .. so create a new one and copy from that
 docker cp $CONTAINER:/module/ output_files
-#mv -f ./output_files/* .
-rsync -av ./output_files/ .
+mv -fuv ./output_files/* .
+#rsync -av ./output_files/ .
 rm -rf ./output_files/*/
 #docker rm $CONTAINER
 """
