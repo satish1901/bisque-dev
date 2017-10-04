5bf13b2c999f1219f64312ab9deff6122a5d284a
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2017-10-03 17:20:15 -0700

    IS: added microcaching for resources and blobs

--- bqserver/bq/image_service/controllers/resource_cache.py
+++ bqserver/bq/image_service/controllers/resource_cache.py
@@ -1,6 +1,6 @@
-""" 
-ResourceCache used to cache resources to speed up retrieval of images without constantly 
-asking data service for meta-data view of a resource. It is required to have a fast etag 
+"""
+ResourceCache used to cache resources to speed up retrieval of images without constantly
+asking data service for meta-data view of a resource. It is required to have a fast etag
 lookup on a data service in order to validate the cache state.
 """
 
@@ -10,16 +10,86 @@ __copyright__ = "Center for BioImage Informatics, University California, Santa B
 
 import sys
 import logging
+from datetime import datetime
 
 from bq import data_service
 from bq import blob_service
 from bq.core import identity
+
+import bq.util.io_misc as misc
 
 import logging
 log = logging.getLogger('bq.image_service.cache')
 
 ################################################################################
 # Resource and Blob caching
+################################################################################
+
+class ResourceDescriptor(object):
+    '''cached and parsed descriptor of a system resource'''
+
+    valid_period = 15000 # in ms
+
+    def __init__(self, uniq):
+        self.uniq = uniq
+        self.ts_resource = None
+        self.resource = None
+        self.meta = None
+        self.ts_files = None
+        self.files = None
+
+    def validate(self):
+        if self.ts_resource is not None:
+            diff = datetime.now() - self.ts_resource
+            ms = diff.total_seconds()*1000.0
+            if ms>self.valid_period:
+                self.ts_resource = None
+                self.resource = None
+                self.meta = None
+
+        if self.ts_files is not None:
+            diff = datetime.now() - self.ts_files
+            ms = diff.total_seconds()*1000.0
+            if ms>self.valid_period:
+                self.ts_files = None
+                self.files = None
+
+    def get_resource(self):
+        self.validate()
+        if self.resource is not None:
+            return self.resource
+        self.resource = data_service.resource_load (uniq=self.uniq, view='image_meta')
+        self.ts_resource = datetime.now()
+        return self.resource
+
+    def get_metadata(self):
+        self.get_resource()
+
+        meta = None
+        try:
+            meta = self.resource.xpath('tag[@type="image_meta"]')[0]
+            meta = dict((i.get('name'), misc.safetypeparse(i.get('value'))) for i in meta.xpath('tag'))
+            if len(meta)==0:
+                meta=None
+        except (AttributeError, IndexError):
+            meta = None
+        self.meta = meta
+        return self.meta
+
+    def get_blobs(self):
+        self.validate()
+        if self.files is not None:
+            # dima: do file existence check here
+            # re-request blob service if unavailable
+            return self.files
+        self.get_resource()
+        self.files = blob_service.localpath(self.uniq, resource=self.resource)
+        self.ts_files = datetime.now()
+        return self.files
+
+
+################################################################################
+# ResourceCache
 ################################################################################
 
 class ResourceCache(object):
@@ -34,40 +104,18 @@ class ResourceCache(object):
             self.d[user] = {}
         d = self.d[user]
         if ident not in d:
-            d[ident] = {}
-
-        #etag = r.get('etag', None)
-        #dima: check if etag changed in data_service
-        #if too old
-        #    d[ident] = {}
-
+            d[ident] = ResourceDescriptor(ident)
         return d[ident]
 
     def get_resource(self, ident):
-        r = self.get_descriptor(ident)
+        d = self.get_descriptor(ident)
+        return d.get_resource()
 
-        # load from cache
-        # if 'resource' in r:
-        #     return r.get('resource')
-
-        resource = data_service.resource_load (uniq = ident, view='image_meta')
-        if resource is not None:
-            r['resource'] = resource
-        return resource
+    def get_meta(self, ident):
+        d = self.get_descriptor(ident)
+        return d.get_metadata()
 
     def get_blobs(self, ident):
-        r = self.get_descriptor(ident)
-
-        # load from cache
-        # if 'blobs' in r:
-        #    blobs = r.get('blobs')
-        #    # dima: do file existence check here
-        #    # re-request blob service if unavailable
-        #    return blobs
-
-        resource = r['resource']
-        blobs = blob_service.localpath(ident, resource=resource)
-        if blobs is not None:
-            r['blobs'] = blobs
-        return blobs
+        d = self.get_descriptor(ident)
+        return d.get_blobs()
 
