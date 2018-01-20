aeef970c00481c71cb245eed98f27d8d9a86fb7b
Kristian Kvilekval <kris@cs.ucsb.edu>
2018-01-20 11:34:30 -0800

    upload/download queue expiremental

--- bqcore/bq/util/s3_handler.py
+++ bqcore/bq/util/s3_handler.py
@@ -2,11 +2,12 @@ import os
 import logging
 import shutil
 #import math
-
+import time
 
 import boto3
 #from boto.s3.key import Key
 import botocore
+from tg import config
 
 #from filechunkio import FileChunkIO
 
@@ -16,6 +17,7 @@ from bq.util.copylink import copy_link
 from bq.util.locks import Locks, FileLocked
 from bq.util.timer import Timer
 from bq.util.sizeoffmt import sizeof_fmt
+from bq.util.converters import asbool
 
 class S3Error(Exception):
     pass
@@ -26,30 +28,62 @@ log = logging.getLogger('bq.blobs.storage.s3')
 #    "Read an s3 url, return a bucket and key"
 #    pass
 
+s3q=None
+if asbool (config.get('bisque.s3queue')):
+    from rq import Queue
+    from redis import Redis
+    s3q = Queue ("S3", connection = Redis())
+
+
+def rate_str (cache_filename, t):
+    size_bytes = os.path.getsize (cache_filename)
+    return "%s %s in %s  (%s)/s" % ( cache_filename, sizeof_fmt(size_bytes), t.interval, sizeof_fmt (size_bytes/t.interval))
+
+def s3_download(bucket, key, cache_filename, creds, blocking):
+    s3_client = boto3.client ('s3', **creds)
+
+    with Locks (None, cache_filename, failonexist=True) as l:
+        if l.locked is True:
+            with Timer () as t:
+                s3_client.download_file (bucket, key, cache_filename)
+            log.info("S3 Downloaded %s", rate_str (cache_filename, t))
+
+    if cache_filename is not None and os.path.exists(cache_filename):
+        with Locks (cache_filename, failonread = (not blocking)) as l:
+            if l.locked is False:
+                return None
+            return cache_filename
+    return None
+
+def s3_upload (bucket, key, cache_filename, creds):
+    s3_client = boto3.client('s3', **creds)
+    with Locks (cache_filename):
+        with Timer () as t:
+            s3_client.upload_file(cache_filename, bucket, key)
+    size_bytes = os.path.getsize (cache_filename)
+    log.info("S3 Uploaded %s", rate_str(cache_filename, t))
 
 def s3_cache_fetch(bucket, key, cache, creds, blocking):
 
     cache_filename = os.path.join(cache, key)
-    if not os.path.exists(cache_filename):
-        _mkdir(os.path.dirname(cache_filename))
-        s3_client = boto3.client ('s3', **creds)
-        #k = Key(bucket)
-        #k.key = key
-        with Locks (None, cache_filename, failonexist=True) as l:
-            if l.locked is True:
-                with Timer () as t:
-                    s3_client.download_file (bucket, key, cache_filename)
-                size_bytes = os.path.getsize (cache_filename)
-                log.info("S3 Downloaded %s %s in %s  (%s)/s",
-                         cache_filename, sizeof_fmt(size_bytes), t.interval, sizeof_fmt (size_bytes/t.interval))
-                #k.get_contents_to_filename(cache_filename)
-
-        if cache_filename is not None and os.path.exists(cache_filename):
-            with Locks (cache_filename, failonread = (not blocking)) as l:
-                if l.locked is False:
-                    raise FileLocked
-                return cache_filename
-        return None
+    if os.path.exists(cache_filename):
+        return cache_filename
+    _mkdir(os.path.dirname(cache_filename))
+    if s3q and not blocking:
+        log.debug ("Queuing download of %s", cache_filename)
+        job = s3q.enqueue(s3_download, bucket, key, cache_filename, creds, blocking, timeout="6h")
+        for ix in range (5):
+            if job.result == None:
+                time.sleep(1)
+            else:
+                return job.result # cache_filename
+        # jobs wait has time out.
+        log.debug ("download timedout")
+        raise FileLocked
+    else:
+        cache_filename = s3_download( bucket, key, cache_filename, creds, blocking)
+        if cache_filename is None and not blocking:
+            raise FileLocked
 
     return cache_filename
 
@@ -67,14 +101,10 @@ def s3_cache_save(f, bucket, key, cache, creds):
         with open(cache_filename, 'wb') as fw:
             shutil.copyfileobj(f, fw)
 
-    s3_client = boto3.client('s3', **creds)
-    with Locks (cache_filename):
-        with Timer () as t:
-            s3_client.upload_file(cache_filename, bucket, key)
-        size_bytes = os.path.getsize (cache_filename)
-        log.info("S3 Uploaded %s %s in %s  (%s)/s",
-                 cache_filename, sizeof_fmt(size_bytes), t.interval, sizeof_fmt (size_bytes/t.interval))
-
+    if s3q:
+        s3q.enqueue (s3_upload  , bucket, key, cache_filename, creds, timeout="6h")
+    else:
+        s3_upload( bucket, key, cache_filename, creds)
 
     # file_size = os.path.getsize(cache_filename)
     # if file_size < 60 * 1e6:
