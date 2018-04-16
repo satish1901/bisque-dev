2715e83945d7a926cd314a13a39b3765a6710e3c
Dmitry Fedorov <dima@dimin.net>
2018-04-16 16:46:41 -0700

    viewer: added slide labels and previews

--- bqcore/bq/core/public/js/viewer/imginfobar.js
+++ bqcore/bq/core/public/js/viewer/imginfobar.js
@@ -36,7 +36,7 @@ ImgInfoBar.prototype.updateImage = function () {
     var viewer = this.viewer,
         view = viewer.current_view,
         dim = view.imagedim,
-        imgphys = viewer.imagephys,
+        phys = viewer.imagephys,
         surf = viewer.viewer_controls_surface ? viewer.viewer_controls_surface : this.parentdiv,
         params = viewer.parameters || {};
 
@@ -66,7 +66,7 @@ ImgInfoBar.prototype.updateImage = function () {
         if (dim.z>1) s += ' Z:'+dim.z;
         if (dim.t>1) s += ' T:'+dim.t;
         s += ' ch: '+ dim.ch;
-        if (imgphys && imgphys.pixel_depth) s += '/'+ imgphys.pixel_depth +'bits';
+        if (phys && phys.pixel_depth) s += '/'+ phys.pixel_depth +'bits';
         s += ' Scale: '+ view.scale*100 +'%';
         this.infobar.innerHTML = s;
     }
@@ -78,4 +78,80 @@ ImgInfoBar.prototype.updateImage = function () {
         }
     }
 };
+
+ImgInfoBar.prototype.updateView = function (view) {
+    if (!this.label && this.viewer.viewer_controls_surface) this.createLabel();
+};
+
+ImgInfoBar.prototype.createLabel = function () {
+    if (this.label) return;
+    this.uuid = this.viewer.image.resource_uniq;
+    this.phys = this.viewer.imagephys;
+
+    if (this.phys.previews) {
+        this.label = true;
+        this.preview = Ext.create('BQ.viewer.LabelView', {
+            renderTo: this.viewer.viewer_controls_surface || this.viewer.imagediv,
+            cls: 'preview',
+            uri: Ext.String.format('/image_service/{0}?slice=label:0&depth=8,d,u&rotate=-90&format=jpeg', this.uuid),
+        });
+    }
+
+    if (this.phys.labels) {
+        this.label = Ext.create('BQ.viewer.LabelView', {
+            renderTo: this.viewer.viewer_controls_surface || this.viewer.imagediv,
+            cls: 'label',
+            uri: Ext.String.format('/image_service/{0}?slice=preview:0&depth=8,d,u&format=jpeg', this.uuid),
+        });
+    }
+
+};
+
+//-----------------------------------------------------------------------
+// BQ.editor.GraphicalSelector - Graphical annotations menu
+//-----------------------------------------------------------------------
+
+Ext.define('BQ.viewer.LabelView', {
+    extend: 'BQ.viewer.MenuButton',
+    alias: 'widget.viewer_label_view',
+    componentCls: 'preview_button',
+
+    afterRender : function() {
+        this.callParent();
+        this.getEl().dom.style.backgroundImage = Ext.String.format("url('{0}')", this.uri);
+    },
+
+    createMenu: function() {
+        if (this.menu) return;
+        var buttons = [],
+            el = this.getEl(),
+            offset = el.getY(),
+            h = 90;
+
+        this.menu = Ext.create('Ext.tip.ToolTip', {
+            target: el,
+            anchor: 'right',
+            anchorToTarget: true,
+            anchorOffset: 5,
+            cls: 'bq-viewer-menu label',
+            width: 650,
+            height: 300,
+            //height: h,
+            autoHide: true,
+            shadow: false,
+            closable: false,
+            layout: 'fit',
+            items: [{
+                xtype: 'component',
+                cls: 'preview_image',
+                style: Ext.String.format("background-image: url({0});", this.uri),
+            }],
+        });
+    },
+
+    onPreferences: function() {
+        //this.auto_hide = BQ.Preferences.get('user','Viewer/gobjects_editor_auto_hide', true);
+    },
+
+});
 
