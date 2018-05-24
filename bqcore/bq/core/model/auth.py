11d30369f3c08df80234f5ef9545ed5c82b392f8
Kristian Kvilekval <kris@cs.ucsb.edu>
2018-05-24 09:35:44 -0700

    pre_ping recipe for SA 1.2 (handle DB restarts) site.cfg change required!

--- bqcore/bq/core/model/auth.py
+++ bqcore/bq/core/model/auth.py
@@ -60,40 +60,39 @@ def do_begin(conn):
         log.debug ("SQLITE: Begin Transaction")
 
 # http://docs.sqlalchemy.org/en/latest/core/pooling.html#pool-disconnects-pessimistic
-@event.listens_for(Engine, "engine_connect")
-def ping_connection(connection, branch):
-    if branch:
-        # "branch" refers to a sub-connection of a connection,
-        # we don't want to bother pinging on these.
-        return
-
-    # turn off "close with result".  This flag is only used with
-    # "connectionless" execution, otherwise will be False in any case
-    save_should_close_with_result = connection.should_close_with_result
-    connection.should_close_with_result = False
-
-    try:
-        # run a SELECT 1.   use a core select() so that
-        # the SELECT of a scalar value without a table is
-        # appropriately formatted for the backend
-        connection.scalar(select([1]))
-    except sa.exc.DBAPIError as err:
-        # catch SQLAlchemy's DBAPIError, which is a wrapper
-        # for the DBAPI's exception.  It includes a .connection_invalidated
-        # attribute which specifies if this connection is a "disconnect"
-        # condition, which is based on inspection of the original exception
-        # by the dialect in use.
-        if err.connection_invalidated:
-            # run the same SELECT again - the connection will re-validate
-            # itself and establish a new connection.  The disconnect detection
-            # here also causes the whole connection pool to be invalidated
-            # so that all stale connections are discarded.
-            connection.scalar(select([1]))
-        else:
-            raise
-    finally:
-        # restore "close with result"
-        connection.should_close_with_result = save_should_close_with_result
+# http://docs.sqlalchemy.org/en/latest/core/pooling.html#disconnect-handling-pessimistic
+#@event.listens_for(Engine, "engine_connect")
+# def ping_connection(connection, branch):
+#     if branch:
+#         # "branch" refers to a sub-connection of a connection,
+#         # we don't want to bother pinging on these.
+#         return
+#     # turn off "close with result".  This flag is only used with
+#     # "connectionless" execution, otherwise will be False in any case
+#     save_should_close_with_result = connection.should_close_with_result
+#     connection.should_close_with_result = False
+#     try:
+#         # run a SELECT 1.   use a core select() so that
+#         # the SELECT of a scalar value without a table is
+#         # appropriately formatted for the backend
+#         connection.scalar(select([1]))
+#     except sa.exc.DBAPIError as err:
+#         # catch SQLAlchemy's DBAPIError, which is a wrapper
+#         # for the DBAPI's exception.  It includes a .connection_invalidated
+#         # attribute which specifies if this connection is a "disconnect"
+#         # condition, which is based on inspection of the original exception
+#         # by the dialect in use.
+#         if err.connection_invalidated:
+#             # run the same SELECT again - the connection will re-validate
+#             # itself and establish a new connection.  The disconnect detection
+#             # here also causes the whole connection pool to be invalidated
+#             # so that all stale connections are discarded.
+#             connection.scalar(select([1]))
+#         else:
+#             raise
+#     finally:
+#         # restore "close with result"
+#         connection.should_close_with_result = save_should_close_with_result
 
 __all__ = ['User', 'Group', 'Permission', 'HashPassword', 'FreeTextPassword']
 
